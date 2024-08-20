7e1e8c17b303e582995de26e1f3fa424289d24cf
SujonHasan <sujonhasan171@gmail.com>
2024-08-20 23:51:12 +0600

    Error Handler middleware added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
+const config = require("../config/config");
 const ApiError = require("../utils/apiError");
 const authRouter = require("./auth.route");
 const guestRouter = require("./guest.route");
 const httpStatus = require('http-status');
+const apiResponse = require('../utils/apiResponse');
 
 
 
@@ -14,6 +16,13 @@ const initRoutes = (app)=> {
     app.use((req, res, next) => {
         const error = new ApiError(httpStatus.NOT_FOUND);
         return next(error);
+    })
+
+    app.use((err, req, res, next) => {
+
+        const status = err.statusCode || res.statusCode || 500;
+        const stack = config.env !== "production" ? err?.stack : {}
+        return apiResponse(res, status, {message: err.message}, stack)
     })
 
 } 
