87fdb428c15608adcadb6ec76b94cbdc66edd348
SujonHasan <sujonhasan171@gmail.com>
2024-09-09 23:02:31 +0600

    package modified

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const express = require('express');
 const cors = require('cors');
+const express = require('express');
 const config = require('./config/config');
 const initRoutes = require('./routes');
 const passport = require('passport');   
@@ -7,15 +7,15 @@ const passportJwtInt = require('./config/passport.jwt');
 
 const app = express();
 
-app.use(express.json({limit: "2mb"}));
-app.use(express.urlencoded({extended: true, limit: "2mb"}))
-app.use(passport.initialize());
-passport.use('jwt', passportJwtInt);
-
 app.use(cors({
     origin: config.corsOrigins,
     Credential: true
 }))
+
+app.use(express.json({limit: "2mb"}));
+app.use(express.urlencoded({extended: true, limit: "2mb"}))
+app.use(passport.initialize());
+passport.use('jwt', passportJwtInt);
 
 initRoutes(app);
 
--- src/seeds/product.seed.js
+++ src/seeds/product.seed.js
@@ -6,9 +6,8 @@ setTimeout( async() => {
     const productContent = {
 
         title:  "test-Product-2",
-        productId: "abc124",
+        productId: "abc125",
         description: {
-            short: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever",
             long: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
         },
         price: {
