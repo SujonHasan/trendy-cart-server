013e18c4e3bef6af3dbca263b58999a8a31502b9
SujonHasan <sujonhasan171@gmail.com>
2024-09-12 18:51:01 +0600

    get user api updated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,13 @@
+const { Usermodel } = require("../models/user.model");
 const apiResponse = require("../utils/apiResponse");
 const catchAsync = require("../utils/catchAsync");
 const httpStatus = require('http-status');
 
 const getUser = catchAsync( async (req, res) => {
     
-    apiResponse(res, httpStatus.OK, {data: req.user});
+    const user = await Usermodel.findById({_id: req.user._id});
+        
+    apiResponse(res, httpStatus.OK, {data: user});
 } )
 
 module.exports = {
