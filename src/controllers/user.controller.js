dcedaf96e40ebdee11f2b2e73cf0e2ed200eda38
SujonHasan <sujonhasan171@gmail.com>
2024-09-13 21:59:07 +0600

    modified user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,14 +1,10 @@
-const { Usermodel } = require("../models/user.model");
 const apiResponse = require("../utils/apiResponse");
 const catchAsync = require("../utils/catchAsync");
 const httpStatus = require('http-status');
 
 const getUser = catchAsync( async (req, res) => {
     
-    const {_id} = req.params;
-
-    const user = await Usermodel.findById({_id});
-    apiResponse(res, httpStatus.OK, {data: user});
+    apiResponse(res, httpStatus.OK, {data: req.user});
 } )
 
 module.exports = {
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,6 +4,6 @@ const { isAuthenticated } = require('../middlewares/auth.middleware');
 
 const router = express.Router();
 
-router.get('/:_id', getUser)
+router.get('/', isAuthenticated, getUser)
 
 module.exports = router;
